ef6bd3e4eee03376f6079ed3e0a1ecc066f3edec
Sam Minot <sminot@gmail.com>
2024-12-04 09:26:28 -0800

    If no images are selected, include everything from the selected datasets

--- .cirro/preprocess.py
+++ .cirro/preprocess.py
@@ -9,15 +9,24 @@ if __name__ == '__main__':
     # Get the images which were selected as inputs
     inputs = ds.params.get("inputs")
 
-    # Make sure that some images were selected
-    if inputs is None:
-        raise ValueError("No input images found")
-    
+    # If no input images were selected, include all .ims files in the dataset
+    if inputs is None or len(inputs) == 0:
+        ds.logger.info("Selecting all images as inputs")
+        inputs = [
+            f for f in ds.files["file"] if f.endswith(".ims")
+        ]
+        if len(inputs) == 0:
+            raise ValueError("No .ims files found in dataset")
+
     # If the images are a list, collapse into a comma delimited string
-    if inputs[0] == '[':
+    if isinstance(inputs, str) and inputs[0] == '[':
         inputs = json.loads(inputs)
     if isinstance(inputs, list):
         inputs = ",".join(inputs)
+
+    n_inputs = len(inputs.split(","))
+
+    ds.logger.info(f"Selected {n_inputs:,} inputs")
 
     # Add the updated inputs to the parameters
     ds.add_param("inputs", inputs, overwrite=True)
